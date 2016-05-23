9f58e0693fb280288b892eba1716503155545f91
Test <cqk87020@laoeq.com>
2016-05-23 19:46:26 +0300

    test

--- app/src/main/java/com/warg/moneyflow/dialogs/AddNewExpencyDialog.java
+++ app/src/main/java/com/warg/moneyflow/dialogs/AddNewExpencyDialog.java
@@ -39,6 +39,8 @@ public class AddNewExpencyDialog extends DialogFragment {
         return builder.create();
     }
 
+    //test
+
     private void addNewExpency() {
         Toast.makeText(getActivity(), "Add from dialog", Toast.LENGTH_SHORT).show();
         MyIntentService.startActionInsertExpency(getActivity(), "Expense", 120);
